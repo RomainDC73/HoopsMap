e9d34089facee47cfa00f0c8f5f3436d15e73edd
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-06-18 11:17:38 +0200

    leaflet first tests

--- js/map.js
+++ js/map.js
@@ -1,7 +1,39 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-console.log('hey');
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f0f',
+    fillOpacity: 0.5,
+    radius: 200
+}).addTo(map);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("Yo this a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+    
+map.on('click', onMapClick);
