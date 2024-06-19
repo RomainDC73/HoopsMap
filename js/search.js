86023d0752e0fb25c587810f56daf260a4444f31
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-06-19 09:36:03 +0200

    no lat long in search bar result

--- js/search.js
+++ js/search.js
@@ -4,30 +4,32 @@ const searchButton = document.getElementById('searchButton');
 
 
 locationInput.addEventListener('input', function() {
-    var input = this.value;
+    const input = this.value;
     if (input.length > 2) {
       fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${input}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
           locationList.innerHTML = '';
           data.forEach(location => {
-            var option = document.createElement('option');
-            option.value = `${location.display_name} (${location.lat}, ${location.lon})`;
+            const option = document.createElement('option');
+            option.value = `${location.display_name})`;
             locationList.appendChild(option);
           });
         });
+
     } else {
       locationList.innerHTML = '';
     }
   });
 
   searchButton.addEventListener('click', function() {
-    var selectedLocation = locationInput.value.split(' (')[0];
+    const selectedLocation = locationInput.value.split(' (')[0];
     fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${selectedLocation}`)
       .then(response => response.json())
       .then(data => {
         if (data.length > 0) {
-          var location = data[0];
+          const location = data[0];
           map.setView([location.lat, location.lon], 13);
         }
       });
