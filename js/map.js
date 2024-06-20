70dae827c3b757c8f4937587a8e4ab9ef087c153
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-06-20 10:40:11 +0200

    map tiler vStreets added

--- js/map.js
+++ js/map.js
@@ -1,7 +1,9 @@
+
 const map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=v9Y0wyS0E3eJC9u7PByU', {
+  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  maxZoom: 20
 }).addTo(map);
 
 if (navigator.geolocation) {
