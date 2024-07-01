e7e9e7bc29aba7f344e01faf5dbdfdaefb1ae979
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-07-01 11:47:14 +0200

    satellite + streets name view

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 const map = L.map('map').setView([46.35546, 2.36225], 6);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9tZGMiLCJhIjoiY2x5MnJ1NzJmMDBpeTJpczNnY3hmeWF3bCJ9.TXQcRxl8KOcll6zuEafSAA', {
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9tZGMiLCJhIjoiY2x5MnJ1NzJmMDBpeTJpczNnY3hmeWF3bCJ9.TXQcRxl8KOcll6zuEafSAA', {
   attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-  maxZoom: 20
+  maxZoom: 20,
+  tileSize: 512,
+  zoomOffset: -1
 }).addTo(map);
 
 const searchInThisAreaButton = document.getElementById('searchInThisAreaButton');
@@ -28,3 +30,5 @@ if (navigator.geolocation) {
   } else {
     console.log("La géolocalisation n'est pas prise en charge par ce navigateur.");
   }
+
+  
