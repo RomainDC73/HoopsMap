47500960ed3c0eefe101dd3c6385975822335b7a
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-07-18 10:30:04 +0200

    show geoloc icon with feather icon

--- js/geolocate.js
+++ js/geolocate.js
@@ -1,22 +1,40 @@
 const geoLocateButton = document.getElementById('geoLocateButton');
-function geolocate(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const { latitude, longitude } = position.coords;
-                map.setView([latitude, longitude], 14);
-                L.marker([latitude, longitude]).addTo(map);
-                updateBasketballCourts(latitude, longitude);
-                locationInput.value = '';
-            },
-            error => {
-                console.error("Erreur de géolocalisation : ", error);
-            }
-            
-        );
-    } else {
-        console.error("La géolocalisation n'est pas disponible");
-    }
+
+const geolocIcon = L.divIcon({
+  html: '<i data-feather="arrow-down-circle"></i>',
+  className: 'show-geoloc-icon', // Classe personnalisée pour styliser l'icône
+  iconSize: [38, 95],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76], // Point de l'icône correspondant à la position du popup
+});
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        map.setView([latitude, longitude], 14);
+
+        // Ajouter un marqueur avec une icône personnalisée à la position géolocalisée
+        const geolocMarker = L.marker([latitude, longitude], { icon: geolocIcon }).addTo(map);
+        
+        // Optionnel : Ajouter un popup au marqueur
+        geolocMarker.bindPopup("Vous êtes ici").openPopup();
+
+        updateBasketballCourts(latitude, longitude);
+        locationInput.value = ''; // Effacer le champ d'entrée de localisation si nécessaire
+
+        // Mettre à jour les icônes Feather
+        feather.replace();
+
+      },
+      error => {
+        console.error("Erreur de géolocalisation : ", error);
+      }
+    );
+  } else {
+    console.error("La géolocalisation n'est pas disponible");
+  }
 }
 
 geoLocateButton.addEventListener('click', geolocate);
